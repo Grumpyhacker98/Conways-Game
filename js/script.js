279a4be28520b921d66f4adbefcfa11bc5569908
Max Lewis <max.a.lewis98@gmail.com>
2020-12-21 16:26:52 -0700

    added time button, and a glider gun creation if the map is created large enough

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var timerVar;
 
 var editArr = [];
 
-var mapSize = 20
+var mapSize = $("#gameSize").val()
 
 var clickSound = new sound("./sound/click.mp3")
 
@@ -23,6 +23,24 @@ function sound(src) {
     }
 }
 
+// click functions
+$("#start").on("click", function () {
+    startGrid()
+})
+
+$("#unpause").on("click", function () {
+    clearInterval(timerVar)
+    let time = parseFloat($("#gameTime").val()) * 1000;
+    timerVar = setInterval(gameLifeCycle, time);
+})
+
+$("#pause").on("click", function () {
+    clearInterval(timerVar)
+})
+
+$("#test").on("click", function () {
+    // console.log($("#mapLength").val())
+})
 
 function clickCell(cell) {
     if (cell.getAttribute("alive") === "true") {
@@ -34,8 +52,9 @@ function clickCell(cell) {
     }
 }
 
+
 function startGrid() {
-    mapSize = $("#mapSize").val()
+    mapSize = $("#gameSize").val()
 
     grid.innerHTML = "";
     clearInterval(timerVar)
@@ -50,32 +69,120 @@ function startGrid() {
         }
     }
 
-    // timerVar = setInterval(gameLifeCycle, 1000);
+    let time = parseFloat($("#gameTime").val()) * 1000;
+    timerVar = setInterval(gameLifeCycle, time);
 
-    // create some example lifeforms
+    // some examples
 
     // static lifeform
-    grid.rows[1].cells[2].classList.add("purple")
-    grid.rows[1].cells[2].setAttribute("alive", true)
+    if (mapSize >= 6) {
+        grid.rows[1].cells[2].classList.add("purple")
+        grid.rows[1].cells[2].setAttribute("alive", true)
 
-    grid.rows[2].cells[1].classList.add("purple")
-    grid.rows[2].cells[1].setAttribute("alive", true)
+        grid.rows[2].cells[1].classList.add("purple")
+        grid.rows[2].cells[1].setAttribute("alive", true)
 
-    grid.rows[2].cells[3].classList.add("purple")
-    grid.rows[2].cells[3].setAttribute("alive", true)
+        grid.rows[2].cells[3].classList.add("purple")
+        grid.rows[2].cells[3].setAttribute("alive", true)
 
-    grid.rows[3].cells[2].classList.add("purple")
-    grid.rows[3].cells[2].setAttribute("alive", true)
+        grid.rows[3].cells[2].classList.add("purple")
+        grid.rows[3].cells[2].setAttribute("alive", true)
+
+    }
 
     // occilating lifeform
-    grid.rows[2].cells[9].classList.add("purple")
-    grid.rows[2].cells[9].setAttribute("alive", true)
+    if (mapSize >= 20) {
+        grid.rows[2].cells[9].classList.add("purple")
+        grid.rows[2].cells[9].setAttribute("alive", true)
 
-    grid.rows[2].cells[7].classList.add("purple")
-    grid.rows[2].cells[7].setAttribute("alive", true)
+        grid.rows[2].cells[7].classList.add("purple")
+        grid.rows[2].cells[7].setAttribute("alive", true)
 
-    grid.rows[2].cells[8].classList.add("purple")
-    grid.rows[2].cells[8].setAttribute("alive", true)
+        grid.rows[2].cells[8].classList.add("purple")
+        grid.rows[2].cells[8].setAttribute("alive", true)
+
+    }
+
+    // gosper glider gun
+    if (mapSize >= 40) {
+        // left box
+        grid.rows[10].cells[1].classList.add("purple")
+        grid.rows[10].cells[1].setAttribute("alive", true)
+        grid.rows[10].cells[2].classList.add("purple")
+        grid.rows[10].cells[2].setAttribute("alive", true)
+        grid.rows[11].cells[1].classList.add("purple")
+        grid.rows[11].cells[1].setAttribute("alive", true)
+        grid.rows[11].cells[2].classList.add("purple")
+        grid.rows[11].cells[2].setAttribute("alive", true)
+        // left occilator
+        grid.rows[10].cells[11].classList.add("purple")
+        grid.rows[10].cells[11].setAttribute("alive", true)
+        grid.rows[11].cells[11].classList.add("purple")
+        grid.rows[11].cells[11].setAttribute("alive", true)
+        grid.rows[12].cells[11].classList.add("purple")
+        grid.rows[12].cells[11].setAttribute("alive", true)
+        grid.rows[9].cells[12].classList.add("purple")
+        grid.rows[9].cells[12].setAttribute("alive", true)
+        grid.rows[13].cells[12].classList.add("purple")
+        grid.rows[13].cells[12].setAttribute("alive", true)
+        grid.rows[8].cells[13].classList.add("purple")
+        grid.rows[8].cells[13].setAttribute("alive", true)
+        grid.rows[14].cells[13].classList.add("purple")
+        grid.rows[14].cells[13].setAttribute("alive", true)
+        grid.rows[8].cells[14].classList.add("purple")
+        grid.rows[8].cells[14].setAttribute("alive", true)
+        grid.rows[14].cells[14].classList.add("purple")
+        grid.rows[14].cells[14].setAttribute("alive", true)
+        grid.rows[11].cells[15].classList.add("purple")
+        grid.rows[11].cells[15].setAttribute("alive", true)
+        grid.rows[9].cells[16].classList.add("purple")
+        grid.rows[9].cells[16].setAttribute("alive", true)
+        grid.rows[13].cells[16].classList.add("purple")
+        grid.rows[13].cells[16].setAttribute("alive", true)
+        grid.rows[10].cells[17].classList.add("purple")
+        grid.rows[10].cells[17].setAttribute("alive", true)
+        grid.rows[11].cells[17].classList.add("purple")
+        grid.rows[11].cells[17].setAttribute("alive", true)
+        grid.rows[12].cells[17].classList.add("purple")
+        grid.rows[12].cells[17].setAttribute("alive", true)
+        grid.rows[11].cells[18].classList.add("purple")
+        grid.rows[11].cells[18].setAttribute("alive", true)
+        // right occilator
+        grid.rows[8].cells[21].classList.add("purple")
+        grid.rows[8].cells[21].setAttribute("alive", true)
+        grid.rows[9].cells[21].classList.add("purple")
+        grid.rows[9].cells[21].setAttribute("alive", true)
+        grid.rows[10].cells[21].classList.add("purple")
+        grid.rows[10].cells[21].setAttribute("alive", true)
+        grid.rows[8].cells[22].classList.add("purple")
+        grid.rows[8].cells[22].setAttribute("alive", true)
+        grid.rows[9].cells[22].classList.add("purple")
+        grid.rows[9].cells[22].setAttribute("alive", true)
+        grid.rows[10].cells[22].classList.add("purple")
+        grid.rows[10].cells[22].setAttribute("alive", true)
+        grid.rows[7].cells[23].classList.add("purple")
+        grid.rows[7].cells[23].setAttribute("alive", true)
+        grid.rows[11].cells[23].classList.add("purple")
+        grid.rows[11].cells[23].setAttribute("alive", true)
+        grid.rows[6].cells[25].classList.add("purple")
+        grid.rows[6].cells[25].setAttribute("alive", true)
+        grid.rows[7].cells[25].classList.add("purple")
+        grid.rows[7].cells[25].setAttribute("alive", true)
+        grid.rows[11].cells[25].classList.add("purple")
+        grid.rows[11].cells[25].setAttribute("alive", true)
+        grid.rows[12].cells[25].classList.add("purple")
+        grid.rows[12].cells[25].setAttribute("alive", true)
+        // right box
+        grid.rows[8].cells[35].classList.add("purple")
+        grid.rows[8].cells[35].setAttribute("alive", true)
+        grid.rows[9].cells[35].classList.add("purple")
+        grid.rows[9].cells[35].setAttribute("alive", true)
+        grid.rows[9].cells[36].classList.add("purple")
+        grid.rows[9].cells[36].setAttribute("alive", true)
+        grid.rows[8].cells[36].classList.add("purple")
+        grid.rows[8].cells[36].setAttribute("alive", true)
+    }
+
 }
 
 function gameLifeCycle() {
@@ -111,7 +218,6 @@ function cellLifeCheck(lat, long) {
 
     if (!cellAlive && adjacentLife === 3) {
         // Births: Each dead cell adjacent to exactly three live neighbors will become live in the next generation.
-        console.log(lat, long, "has been born")
         editArr.push({
             lat: lat,
             long: long,
@@ -119,7 +225,6 @@ function cellLifeCheck(lat, long) {
         })
     } else if (cellAlive && adjacentLife <= 1) {
         // Death by isolation: Each live cell with one or fewer live neighbors will die in the next generation.
-        console.log(lat, long, "has died alone")
         editArr.push({
             lat: lat,
             long: long,
@@ -127,28 +232,20 @@ function cellLifeCheck(lat, long) {
         })
     } else if (cellAlive && adjacentLife <= 3 && adjacentLife >= 2) {
         // Survival: Each live cell with either two or three live neighbors will remain alive for the next generation.
-        console.log(lat, long, "has survived")
-        // editArr.push({
-        //     lat: lat,
-        //     long: long,
-        //     alive: true,
-        // })
     } else if (cellAlive && adjacentLife > 3) {
         // Death by overcrowding: Each live cell with four or more live neighbors will die in the next generation
-        console.log(lat, long, "has died overcrowded")
         editArr.push({
             lat: lat,
             long: long,
             alive: false,
         })
     }
-
 }
 
 function callAdjacent(lat, long) {
-    if (lat > 14 || lat < 0 || long > 14 || long < 0) return 0
-
+    if (lat > mapSize - 1 || lat < 0 || long > mapSize - 1 || long < 0) return 0;
     cell = grid.rows[lat].cells[long]
+
     if (cell.getAttribute("alive") === "true") {
         return 1
     } else {
@@ -170,20 +267,3 @@ function gridEdit(arr) {
 
     }
 }
-
-$("#start").on("click", function () {
-    startGrid()
-})
-
-$("#unpause").on("click", function () {
-    clearInterval(timerVar)
-    timerVar = setInterval(gameLifeCycle, 1000);
-})
-
-$("#pause").on("click", function () {
-    clearInterval(timerVar)
-})
-
-$("#test").on("click", function () {
-    console.log($("#mapLength").val())
-})
